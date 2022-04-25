f7245704984172150a025ead0c32e49d78e88d73
Lilia Vinogradova <89735964+LiliaVin@users.noreply.github.com>
2022-04-25 13:30:48 +0300

    Add files via upload

---
+++ While.js
@@ -0,0 +1,12 @@
+let i_1 = 0;
+let i_2 = 20;
+
+// while(true) {
+//     console.log(i_1 + "= idi kushat")
+//     i_1++
+// }
+
+while(i_1 < 20) {
+    console.log(i_1 + " = idi kushat")
+    i_1++
+}
